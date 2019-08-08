6a969d8ea668d0acc8da65540c7c268806b2b3da
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-08-08 10:44:54 -0700

    Add files via upload

---
+++ DependencyRegistrations.cs
@@ -0,0 +1,23 @@
+﻿using Penguin.DependencyInjection.Abstractions;
+using Penguin.DependencyInjection;
+using Penguin.DependencyInjection.ServiceProviders;
+using Penguin.Web.Mvc.Abstractions;
+using Penguin.Web.Mvc;
+
+namespace Penguin.Cms.Web.Mail.EmailRenderer.DependencyInjection
+{
+    /// <summary>
+    /// Registers dependencies for this assembly
+    /// </summary>
+    public class DependencyRegistrations : IRegisterDependencies
+    {
+        /// <summary>
+        /// Registers dependencies for this assembly
+        /// </summary>
+        public void RegisterDependencies()
+        {
+            Engine.Register<IViewRenderService, ViewRenderService>(typeof(ScopedServiceProvider));
+
+        }
+    }
+}
